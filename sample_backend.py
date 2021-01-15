f0c5dc8e83bce439158171a693aa05f067cd44fc
jrizo15 <j.rizo15@yahoo.com>
2021-01-15 11:10:43 -0800

    Linked the backend with react app frontend.

--- sample_backend.py
+++ sample_backend.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS;
 
 
 app = Flask(__name__)
+CORS(app) # <--- add this line
 
 @app.route('/')
 def hello_world():
